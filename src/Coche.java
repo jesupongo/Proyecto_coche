6fde07e983bd77da0a58ae10b6a620469934cec1
Jesu <jgonlec2206@g.educaand.es>
2024-11-20 08:47:00 +0100

    Cambios en Coche

--- src/Coche.java
+++ src/Coche.java
@@ -25,6 +25,10 @@ public class Coche {
     public void setAnio(int anio) {
         this.anio = anio;
     }
+    public void encender() {
+        System.out.println("El coche está encendido.");
+    }
+
     @Override
     public String toString() {
         return "Coche{" +
